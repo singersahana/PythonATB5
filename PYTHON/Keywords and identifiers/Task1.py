34f618136027013d65cb8535734c527d09f302e9
Sahana H C <sahanahc979@gmail.com>
2024-12-26 20:21:18 +0530

    Part 3

---
+++ PYTHON/Keywords and identifiers/Task1.py
@@ -0,0 +1,14 @@
+# Take 3 Input from the user
+# perform the sub, mul,add,div
+# ------------------------------------------------------------------
+a = int(input("Enter the num1\n"))
+b = int(input("Enter the num2\n"))
+c = int(input("Enter the num3\n"))
+add = a + b + c
+sub = a - b - c
+mul = a * b * c
+div = a / b / c
+print("The addition of 3 num are",add)
+print("The subtraction of 3 num are",sub)
+print("The multiplication of 3 num are",mul)
+print("The division of 3 num are",div)
---
+++ PYTHON/Keywords and identifiers/Task2.py
@@ -0,0 +1,13 @@
+# Take 2 input from the user print the Quotient and Remainder
+# num1 ---> 15
+# num2 --->2
+# Q --->7
+# r --->1
+# -------------------------------------------------------------------
+a = int(input("Enter the num1\n"))
+b = int(input("Enter the num2\n"))
+Quotient = a // b # inorder get the int value we need to specify ---->//
+# inorder to get the floating value ----> /
+remainder = a % b
+print(Quotient)
+print(remainder)
